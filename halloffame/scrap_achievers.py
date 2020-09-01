7d2e8cb258710e79f8d5a730abde461439c74f73
mayur.kanojiya <kanojiyamayur@gmail.com>
2020-09-01 14:26:55 +0530

    feat: created script to scrap 50days of udacity completion

--- halloffame/scrap_achievers.py
+++ halloffame/scrap_achievers.py
@@ -8,7 +8,7 @@ from google.auth.transport.requests import Request
 
 secret_df = pd.read_csv('CONFIDENTIAL')
 ID = secret_df.iloc[0, 0]
-SHEET_NAME = secret_df.iloc[2, 0]
+SHEET_NAME = secret_df.iloc[3, 0]
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
@@ -47,19 +47,24 @@ result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range=SAMPLE_RANGE_NAME).execute()
 values = result.get('values', [])
 
-students = []
+data = []
+slack_mentioned_message = ""
 if not values:
     print('No data found.')
 else:
     print('DATA FOUND !!')
     for row in values:
-        # Print columns A and E, which correspond to indices 0 and 4.
-        # print('%s, %s, %s, %s, %s, %s, %s, %s, %s' % (column[0], column[2], column[4], column[6], column[8], column[10], column[12], column[14], column[16]))
+        # Print rows A and E, which correspond to indices 0 and 4.
         try:
-            for col in range(len(row)):
-                if col in [0.4, 6, 8, 10, 12, 14, 16]:
-                    students.append(row[col])
+            data.append([row[0], row[1], row[2], row[3], row[4], row[5]])
+            slack_mentioned_message += "@"+str(row[3]+",\n")
         except Exception as err:
             print(err)
 
-print(len(students))
+df = pd.DataFrame(data, columns =['date', 'email', 'name', 'slack_name', 'iscomplete', 'comment'])
+
+print(df.head())
+
+print(df.shape)
+print("========")
+print(slack_mentioned_message)
